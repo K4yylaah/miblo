2e6fc4620921812a0ddf97d429cd715badc50ec8
samuel <samuel@mac-cf-23-002.home>
2025-12-17 21:26:22 +0100

    MBL fix : fix test function

--- testUnitaire/test_deposit.py
+++ testUnitaire/test_deposit.py
@@ -6,7 +6,7 @@ from models.model import BankAccount
 
 def test_deposit_money_success(test_engine):
     with Session(test_engine) as session:
-        compte = BankAccount(id=1, solde=100)
+        compte = BankAccount(id=1,user_id=1,solde=100,rib="FR761234598765",is_primary=True,is_closed=False)
         session.add(compte)
         session.commit()
     result = depositMoney(1, 50, test_engine)
