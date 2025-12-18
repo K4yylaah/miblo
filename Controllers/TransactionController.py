4cf1b5193f291bc6d6cd423428c5be6221aec2ce
samuel <samuel@mac-cf-23-002.home>
2025-12-18 19:45:36 +0100

    MBL UPDATE : update function avoidable check name

--- Controllers/TransactionController.py
+++ Controllers/TransactionController.py
@@ -4,7 +4,7 @@ from models.model import Transactions, BankAccount, User
 from database import engine
 
 
-def is_avoidableCheck(id_transaction, timer):
+def isAvoidableCheck(id_transaction, timer):
     with Session(engine) as session:
         transaction = session.exec(select(Transactions).where(Transactions.id == id_transaction)).first()
         if timer > 5:
